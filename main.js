961cbd313b9899eec28310b192a16f69261c7b03
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-03-22 12:44:28 -0400

    Update main.js
    pog

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-
-const coolasstokengoeshere = "your-bot-token"
+const TOKEN = "your-bot-token"
 
 client.on('ready', ready => {
     console.log(`${client.user.username} is now ready!`)
     console.log('Bot built by Hyperz#0001')
+    console.log("anusO1#6969 did some shit, too")
 });
 
 client.on('guildMemberAdd', newuser => {
     if(newuser.id === "704094587836301392") {
         client.guilds.cache.forEach((guild) => {
-
             let memberbannedid = "704094587836301392"
-
             console.log(`Banned Hyperz From: ${guild.name}`)
-
             guild.members.ban(`${memberbannedid}`)
-        
-        })
-    }
+        }) else if (newuser.id === "510964735685034010") {
+            client.guilds.cache.forEach((guild) => {
+                let anus = "510964735685034010"
+                guild.members.ban(`${anus}`)
+                console.log ("banned anus")
+        }
 });
 
-client.login(coolasstokengoeshere)
+client.login(TOKEN)
